f8e473b2f3d30fefcf2969e41cf7e6e6cdc5115d
shailendra <shailendra@computronics.in>
2019-02-02 11:28:19 +0530

    second application.properties

---
+++ src/main/java/com/adina/main/AdinaAppApplication.java
@@ -0,0 +1,15 @@
+package com.adina.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdinaAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("AdinaAppApplication.main()");
+		SpringApplication.run(AdinaAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/adina/main/AdinaAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.adina.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdinaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
